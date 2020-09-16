e7ea953c2700af5eb11a48603903aea0daa8e2b4
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-09-16 19:43:22 +0300

    setup of nav and main components with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
 import './App.css';
+import Albums from './components/Albums';
+import Artists from './components/Artists';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Playlists from './components/Playlists';
+import SingleAlbum from './components/SingleAlbum';
+import SingleArtist from './components/SingleArtist';
+import SinglePlaylist from './components/SinglePlaylist';
+import SingleSong from './components/SingleSong';
+import Songs from './components/Songs';
 
 function App() {
   return (
     <div className="App">
-      hey there ♥
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/song">
+            <Songs />
+          </Route>
+          <Route exact path="/song/:id">
+            <SingleSong />
+          </Route>
+          <Route exact path="/artist">
+            <Artists />
+          </Route>
+          <Route exact path="/artist/:id">
+            <SingleArtist />
+          </Route>
+          <Route exact path="/album">
+            <Albums />
+          </Route>
+          <Route exact path="/album/:id">
+            <SingleAlbum />
+          </Route>
+          <Route exact path="/playlist">
+            <Playlists />
+          </Route>
+          <Route exact path="/playlist/:id">
+            <SinglePlaylist />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
+    <div style={{ backgroundColor: 'lightblue', color: 'red' }}>
+      <span>MyMusicService </span>
+      <NavLink to="/" activeStyle={{ backgroundColor: 'grey' }}>Home </NavLink>
+      <NavLink to="/song" activeStyle={{ backgroundColor: 'grey' }}>Songs </NavLink>
+      <NavLink to="/artist" activeStyle={{ backgroundColor: 'grey' }}>Artists </NavLink>
+      <NavLink to="/album" activeStyle={{ backgroundColor: 'grey' }}>Albums </NavLink>
+      <NavLink to="/playlist" activeStyle={{ backgroundColor: 'grey' }}>Playlists </NavLink>
+      {/* add search input and component */}
     </div>
   );
 }
